b7e733851c4cb9521c089f923a51651321ac2578
Huy Lai <lai.huy@tamu.edu>
2023-03-02 20:07:43 -0600

    update homework 3

--- Homework 3/assignment3.py
+++ Homework 3/assignment3.py
@@ -1,19 +1,17 @@
-from typeguard import typechecked
-import random
-from sklearn.metrics import roc_auc_score
-from sklearn.linear_model import Ridge, Lasso
-from sklearn.metrics import roc_curve, roc_auc_score
-from typing import Tuple, List, Optional, Any, Callable, Dict, Union
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from typing import Tuple, List, Optional, Any, Callable, Dict, Union
+from sklearn.metrics import roc_curve, roc_auc_score
+from sklearn.linear_model import Ridge, Lasso
+from sklearn.metrics import roc_auc_score
+import random
+from typeguard import typechecked
 
 random.seed(42)
 np.random.seed(42)
-
-# +
 
 
 @typechecked
@@ -21,11 +19,7 @@ def read_data(filename: str) -> pd.DataFrame:
     """
     Read the data from the filename. Load the data it in a dataframe and return it.
     """
-    data: pd.DataFrame = pd.read_csv(filename)
-    data = data.dropna()
-    return data
-
-# +
+    return pd.read_csv(filename)
 
 
 @typechecked
@@ -35,14 +29,17 @@ def data_preprocess(df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.Series]:
     Return the final features and final label in same order
     You may use the same code you submiited for problem 2 of HW2
     """
-    df_numerical: pd.DataFrame = df.select_dtypes(include=['int64', 'float64'])
-    df_categorical: pd.DataFrame = df.select_dtypes(
-        exclude=['int64', 'float64'])
+
+    df = df.dropna()
+    df = df.drop(['Player'], axis=1)
+    feature = df.drop(['NewLeague'], axis=1)
+    df_numerical = feature.select_dtypes(include=['int64', 'float64'])
+    df_categorical = feature.select_dtypes(exclude=['int64', 'float64'])
     df_categorical = pd.get_dummies(df_categorical)
-
-    return pd.concat([df_categorical, df_numerical], axis=1), pd.Series(df['NewLeague'])
-
-# +
+    labels = pd.Series(df['NewLeague'])
+    labels = labels.replace('A', 0)
+    labels = labels.replace('N', 1)
+    return pd.concat([df_categorical, df_numerical], axis=1), labels
 
 
 @typechecked
@@ -53,11 +50,9 @@ def data_split(
     Split 80% of data as a training set and the remaining 20% of the data as testing set
     return training and testing sets in the following order: X_train, X_test, y_train, y_test
     """
-    x_train, x_test, y_train, y_test = train_test_split(
+    X_train, X_test, y_train, y_test = train_test_split(
         features, label, test_size=test_size)
-    return x_train, x_test, y_train, y_test
-
-# +
+    return X_train, X_test,  y_train,  y_test
 
 
 @typechecked
@@ -83,9 +78,8 @@ def train_ridge_regression(
         for lambda_val in lambda_vals:
             ridge = Ridge(alpha=lambda_val, max_iter=max_iter)
             ridge.fit(x_train, y_train)
-            y_pred = ridge.predict(x_test)
-            auc = roc_auc_score(y_test, y_pred)
-            aucs["ridge"].append(auc)
+            auc = roc_auc_score(y_test, ridge.predict(x_test))
+            aucs["ridge"].append({lambda_val: auc})
 
     print("ridge mean AUCs:")
     ridge_aucs = pd.DataFrame(aucs["ridge"])
@@ -95,8 +89,6 @@ def train_ridge_regression(
         ridge_mean_auc[lambda_val] = ridge_auc
         print("lambda:", lambda_val, "AUC:", "%.4f" % ridge_auc)
     return ridge_mean_auc
-
-# +
 
 
 @typechecked
@@ -122,8 +114,8 @@ def train_lasso(
         for lambda_val in lambda_vals:
             lasso = Lasso(alpha=lambda_val, max_iter=max_iter)
             lasso.fit(x_train, y_train)
-            y_pred = lasso.predict(x_test)
-            aucs["ridge"].append(roc_auc_score(y_test, y_pred))
+            auc = roc_auc_score(y_test, lasso.predict(x_test))
+            aucs["lasso"].append({lambda_val: auc})
 
     print("lasso mean AUCs:")
     lasso_mean_auc = {}
@@ -131,9 +123,8 @@ def train_lasso(
     for lambda_val, lasso_auc in zip(lambda_vals, lasso_aucs.mean()):
         lasso_mean_auc[lambda_val] = lasso_auc
         print("lambda:", lambda_val, "AUC:", "%.4f" % lasso_auc)
-    return lasso_mean_auc
 
-# +
+    return lasso_mean_auc
 
 
 @typechecked
@@ -147,11 +138,9 @@ def ridge_coefficients(
     return the tuple consisting of trained Ridge model with alpha as optimal_alpha and the coefficients
     of the model
     """
-    ridge: Ridge = Ridge(alpha=optimal_alpha, max_iter=max_iter)
+    ridge = Ridge(alpha=optimal_alpha, max_iter=max_iter)
     ridge.fit(x_train, y_train)
     return ridge, ridge.coef_
-
-# +
 
 
 @typechecked
@@ -165,11 +154,9 @@ def lasso_coefficients(
     return the tuple consisting of trained Lasso model with alpha as optimal_alpha and the coefficients
     of the model
     """
-    lasso: Lasso = Lasso(alpha=optimal_alpha, max_iter=max_iter)
+    lasso = Lasso(alpha=optimal_alpha, max_iter=max_iter)
     lasso.fit(x_train, y_train)
-    return lasso, lasso.ceof_
-
-# +
+    return lasso, lasso.coef_
 
 
 @typechecked
@@ -181,23 +168,8 @@ def ridge_area_under_curve(
     i.e., model tarined with optimal_aplha
     Finally plot the ROC Curve using false_positive_rate, true_positive_rate as x and y axes calculated from roc_curve
     """
-    y_prob = model_R.predict_proba(x_test)[:, 1]
-
-    fpr, tpr, thresholds = roc_curve(y_test, y_prob)
-    roc_auc = roc_auc_score(y_test, y_prob)
-    plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
-    plt.plot([0, 1], [0, 1], 'k--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver operating characteristic')
-    plt.legend(loc="lower right")
-    plt.show()
-
-    return roc_auc
-
-# +
+    auc = roc_auc_score(y_test, model_R.predict(x_test))
+    return auc
 
 
 @typechecked
@@ -209,12 +181,8 @@ def lasso_area_under_curve(
     i.e., model tarined with optimal_aplha
     Finally plot the ROC Curve using false_positive_rate, true_positive_rate as x and y axes calculated from roc_curve
     """
-    ########################
-    ## Your Solution Here ##
-    ########################
-    pass
-
-# +
+    auc = roc_auc_score(y_test, model_L.predict(x_test))
+    return auc
 
 
 class Node:
@@ -238,8 +206,6 @@ class Node:
         self.split_val = split_val
         self.data = data  # data can be anything! we recommend dictionary with all variables you need
 
-# +
-
 
 class TreeRegressor:
     @typechecked
@@ -257,10 +223,9 @@ class TreeRegressor:
         """
         Build the tree
         """
-        ######################
-        ### YOUR CODE HERE ###
-        ######################
-        pass
+        root = self.get_best_split(self.data)
+        self.split(root, 1)
+        return root
 
     @typechecked
     def mean_squared_error(
@@ -271,30 +236,72 @@ class TreeRegressor:
         left split is a list of rows of a df, rightmost element is label
         return the sum of mse of left split and right split
         """
-        ######################
-        ### YOUR CODE HERE ###
-        ######################
-        pass
+        left_target = left_split[:, -1]
+        left_mean = left_target.mean()
+        left_mse = ((left_target - left_mean) ** 2).sum()
+
+        right_target = right_split[:, -1]
+        right_mean = right_target.mean()
+        right_mse = ((right_target - right_mean) ** 2).sum()
+
+        return (left_mse + right_mse) / len(self.data)
 
     @typechecked
     def split(self, node: Node, depth: int) -> None:
         """
         Do the split operation recursively
         """
-        ######################
-        ### YOUR CODE HERE ###
-        ######################
-        pass
+        left_data, right_data = node.data['data'], node.data['data']
+        left_rows, right_rows = [], []
+        if depth >= self.max_depth or len(left_data) == 0 or len(right_data) == 0:
+            # If we've reached maximum depth or one of the datasets is empty, stop recursion and set node as leaf
+            node.left = Node(split_val=np.mean(left_data[:, -1]), data={'data': left_data})
+            node.right = Node(split_val=np.mean(right_data[:, -1]), data={'data': right_data})
+            return
+        
+        best_split = self.get_best_split(node.data)
+        if best_split is None:
+            # If we couldn't find a valid split, set node as leaf
+            node.left = Node(split_val=np.mean(left_data[:, -1]), data={'data': left_data})
+            node.right = Node(split_val=np.mean(right_data[:, -1]), data={'data': right_data})
+            return
+        
+        index, value = best_split['index'], best_split['value']
+        left_data, right_data = self.one_step_split(index, value, node.data['data'])
+        node.left = Node(split_val=value, data={'data': left_data})
+        node.right = Node(split_val=value, data={'data': right_data})
+        self.split(node.left, depth+1)
+        self.split(node.right, depth+1)
 
     @typechecked
     def get_best_split(self, data: np.ndarray) -> Node:
         """
         Select the best split point for a dataset AND create a Node
         """
-        ######################
-        ### YOUR CODE HERE ###
-        ######################
-        pass
+        best_index, best_value, best_score, best_splits = float('inf'), float('inf'), float('inf'), None
+        num_variables = len(data[0]) - 1  # last column is the target
+
+        for index in range(num_variables):
+            for row in data:
+                left, right = self.one_step_split(index, row[index], data)
+                if len(left) == 0 or len(right) == 0:
+                    continue
+
+                mse = self.mean_squared_error(left, right)
+                if mse < best_score:
+                    best_index, best_value, best_score, best_splits = (
+                        index,
+                        row[index],
+                        mse,
+                        (left, right),
+                    )
+
+        if best_splits is None:
+            # no split made, return the mean
+            return Node(split_val=data[:, -1].mean())
+
+        left, right = [self.build_tree(split) for split in best_splits]
+        return Node(split_val=best_value, data={"index": best_index}, left=left, right=right)
 
     @typechecked
     def one_step_split(
@@ -306,12 +313,10 @@ class TreeRegressor:
         returns the left and right split each as list
         each list has elements as `rows' of the df
         """
-        ######################
-        ### YOUR CODE HERE ###
-        ######################
-        pass
+        left = data[data[:, index] < value]
+        right = data[data[:, index] >= value]
+        return left, right
 
-# +
 
 
 @typechecked
@@ -325,8 +330,6 @@ def compare_node_with_threshold(node: Node, row: np.ndarray) -> bool:
     ######################
     pass
 
-# +
-
 
 @typechecked
 def predict(
@@ -336,8 +339,6 @@ def predict(
     ### YOUR CODE HERE ###
     ######################
     pass
-
-# +
 
 
 class TreeClassifier(TreeRegressor):
@@ -376,10 +377,8 @@ class TreeClassifier(TreeRegressor):
         pass
 
 
-# -
-
 if __name__ == "__main__":
-    # Question 1
+    # # Question 1
     filename = "./Hitters.csv"  # Provide the path of the dataset
     df = read_data(filename)
     lambda_vals = [1e-3, 1e-2, 1e-1, 1, 1e1, 1e2, 1e3]
@@ -389,6 +388,7 @@ if __name__ == "__main__":
         final_features, final_label, 0.2
     )
     ridge_mean_acu = train_ridge_regression(x_train, y_train, x_test, y_test)
+    print(ridge_mean_acu)
     lasso_mean_acu = train_lasso(x_train, y_train, x_test, y_test)
     model_R, ridge_coeff = ridge_coefficients(x_train, y_train, 10)
     model_L, lasso_coeff = lasso_coefficients(x_train, y_train, 0.1)
@@ -397,20 +397,34 @@ if __name__ == "__main__":
     # Plot the ROC curve of the Ridge Model. Include axes labels,
     # legend and title in the Plot. Any of the missing
     # items in plot will result in loss of points.
-    ########################
-    ## Your Solution Here ##
-    ########################
+    predictions = model_R.predict(x_test)
+    fpr, tpr, thresholds = roc_curve(y_test, predictions)
+    plt.plot(fpr, tpr, color='darkorange', lw=2,
+             label='ROC curve (AUC = %0.2f)' % ridge_auc)
 
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC Curve - Ridge')
+    plt.legend(loc="lower right")
+    plt.show()
     lasso_auc = lasso_area_under_curve(model_L, x_test, y_test)
 
     # Plot the ROC curve of the Lasso Model.
     # Include axes labels, legend and title in the Plot.
     # Any of the missing items in plot will result in loss of points.
-    ########################
-    ## Your Solution Here ##
-    ########################
+    predictions = model_L.predict(x_test)
+    fpr, tpr, thresholds = roc_curve(y_test, predictions)
+    plt.plot(fpr, tpr, color='darkorange', lw=2,
+             label='ROC curve (AUC = %0.2f)' % lasso_auc)
 
-    # SUB Q1
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC Curve - Lasso')
+    plt.legend(loc="lower right")
+    plt.show()
+
+    # Tree Regressor stuff
+    csvname = "./noisy_sin_subsample_2.csv"
     data_regress = np.loadtxt(csvname, delimiter=",")
     data_regress = np.array([[x, y] for x, y in zip(*data_regress)])
     plt.figure()
@@ -436,8 +450,7 @@ if __name__ == "__main__":
     plt.ylabel("MSE")
     plt.show()
 
-    # SUB Q2
-    # Place the CSV file in the same directory as this notebook
+    # Tree Classifier stuff
     csvname = "new_circle_data.csv"
     data_class = np.loadtxt(csvname, delimiter=",")
     data_class = np.array([[x1, x2, y] for x1, x2, y in zip(*data_class)])
