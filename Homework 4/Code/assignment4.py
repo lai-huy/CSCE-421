1350291ee211a52837d16199acf71b889e7ee060
Huy Lai <lai.huy@tamu.edu>
2023-03-13 17:38:43 -0500

    update homework 4

--- Homework 4/Code/assignment4.py
+++ Homework 4/Code/assignment4.py
@@ -11,10 +11,8 @@ import random
 from typeguard import typechecked
 
 
-
 random.seed(42)
 np.random.seed(42)
-
 
 # ###############
 # ###############
@@ -28,25 +26,23 @@ from typing import Tuple
 
 @typechecked
 def read_classification_data(file_path: str) -> Tuple[np.array, np.array]:
-    '''
+	'''
 		Read the data from the path.
 		Return the data as 2 np arrays each with shape (number_of_rows_in_dataframe, 1)
 		Order (np.array from first row), (np.array from second row)
-    '''
-    df: pd.DataFrame = pd.read_csv(file_path, header=None)
-    X: np.ndarray = df.iloc[:, 0].to_numpy().reshape(-1, 1)
-    y: np.ndarray = df.iloc[:, 1].to_numpy().reshape(-1, 1)
-    return X, y
-
-
+	'''
+	df: pd.DataFrame = pd.read_csv(file_path, header=None)
+	X: np.ndarray = df.iloc[:, 0].to_numpy().reshape(-1, 1)
+	y: np.ndarray = df.iloc[:, 1].to_numpy().reshape(-1, 1)
+	return X, y
 
 # -
 
 @typechecked
 def sigmoid(s: np.array) -> np.array:
     '''
-      Return the sigmoid of every number in the array s as an array of floating point numbers
-      sigmoid(s)= 1/(1+e^(-s))
+    	Return the sigmoid of every number in the array s as an array of floating point numbers
+    	sigmoid(s)= 1/(1+e^(-s))
     '''
     return 1 / (1 + np.exp(-s))
 
@@ -87,15 +83,17 @@ def cross_entropy_optimizer(w: float, b: float, X: np.array, y: np.array, num_it
         "costs" list contains float type numbers  
     '''
     costs = []
+    weight: float = w
+    offset: float = b
     for i in range(num_iterations):
         y_pred = sigmoid(np.dot(X, w) + b)
         dw: float = np.dot(X.T, (y_pred - y)) / len(y)
         db: float = np.sum(y_pred - y) / len(y)
-        w -= alpha * dw
-        b -= alpha * db
-        cost = cost_function(w, b, X, y)
+        weight -= float(alpha * dw)
+        offset -= float(alpha * db)
+        cost = cost_function(weight, offset, X, y)
         costs.append(cost)
-    return w, b, costs
+    return weight, offset, costs
 
 
 # ###############
@@ -107,8 +105,8 @@ def cross_entropy_optimizer(w: float, b: float, X: np.array, y: np.array, num_it
 @typechecked
 def read_sat_image_data(file_path: str) -> pd.DataFrame:
     '''
-		Input: filepath to a .csv file
-		Output: Return a DataFrame with the data from the given csv file 
+		  Input: filepath to a .csv file
+		  Output: Return a DataFrame with the data from the given csv file 
     '''
     return pd.read_csv(file_path)
 
@@ -140,9 +138,14 @@ def labels_to_binary(y: pd.DataFrame) -> pd.DataFrame:
     Make the lables [1,2,3,4,5] as 0 and [6] as 1
     Return the DataFrame 
     '''
-    ########################
-    ## Your Solution Here ##
-    ########################
+    # Create a copy of the original DataFrame to avoid modifying it
+    y_binary: pd.DataFrame = y.copy()
+    
+    y_binary.loc[y_binary["Class"].isin([1,2,3,4,5]), "Class"] = 0
+    y_binary.loc[y_binary["Class"] == 6, "Class"] = 1
+    
+    # Return the modified DataFrame
+    return y_binary
 
 
 
@@ -152,16 +155,42 @@ def labels_to_binary(y: pd.DataFrame) -> pd.DataFrame:
 # ###############
 # ###############
 
-@typechecked
-def cross_validate_c_vals(X: pd.DataFrame, y: pd.DataFrame, n_folds: int, c_vals: np.array, d_vals: np.array) -> (np.array, np.array):
-    '''
-      Return the matrices (ERRAVGdc, ERRSTDdc) in the same order
-      More details about the imlementation are provided in the main function
-    '''
-    ########################
-    ## Your Solution Here ##
-    ########################
+# +
+from sklearn.svm import SVR
+from sklearn.model_selection import KFold
+from sklearn.metrics import mean_absolute_error
 
+@typechecked
+def cross_validate_c_vals(X: pd.DataFrame, y: pd.DataFrame, n_folds: int, c_vals: np.array, d_vals: np.array) -> Tuple[np.array, np.array]:
+	'''
+		Return the matrices (ERRAVGdc, ERRSTDdc) in the same order
+		More details about the imlementation are provided in the main function
+	'''
+	ERRAVGdc = np.zeros((len(c_vals), len(d_vals)))
+	ERRSTDdc = np.zeros((len(c_vals), len(d_vals)))
+
+	for i, c in enumerate(c_vals):
+		for j, d in enumerate(d_vals):
+			fold_errors = np.zeros(n_folds)
+			svr = SVR(kernel='sigmoid', degree=d, C=c)
+
+			kfold = KFold(n_splits=n_folds, shuffle=True, random_state=42)
+
+			for k, (train_indices, test_indices) in enumerate(kfold.split(X, y)):
+				X_train, y_train = X.iloc[train_indices], y.iloc[train_indices]
+				X_test, y_test = X.iloc[test_indices], y.iloc[test_indices]
+
+				svr.fit(X_train, y_train.values.ravel())
+				y_pred = svr.predict(X_test)
+				fold_errors[k] = mean_absolute_error(y_test, y_pred)
+
+			ERRAVGdc[i, j] = np.mean(fold_errors)
+			ERRSTDdc[i, j] = np.std(fold_errors)
+
+	return ERRAVGdc, ERRSTDdc
+
+
+# -
 
 @typechecked
 def plot_cross_val_err_vs_c(ERRAVGdc: np.array, ERRSTDdc: np.array, c_vals: np.array, d_vals: np.array) -> None:
@@ -169,30 +198,84 @@ def plot_cross_val_err_vs_c(ERRAVGdc: np.array, ERRSTDdc: np.array, c_vals: np.a
      Please write the code in below block to generate the graphs as described in the question.
      Note that the code will not be graded, but the graphs submitted in the report will be evaluated.
     '''
-    ########################
-    ## Your Solution Here ##
-    ########################
+    fig, axs = plt.subplots(len(d_vals), sharex=True, sharey=True, figsize=(10, 10))
+    fig.suptitle('Cross Validation Errors vs. C')
+
+    for d_idx, d in enumerate(d_vals):
+        axs[d_idx].errorbar(c_vals, ERRAVGdc[:,d_idx], yerr=ERRSTDdc[:,d_idx], fmt='o-', capsize=5)
+        axs[d_idx].set_xscale('log')
+        axs[d_idx].set_xlabel('C')
+        axs[d_idx].set_ylabel(f'Degree {d} Error')
+
+    plt.tight_layout()
+    plt.show()
 
 
-################
-################
-# Q3 c
-################
-################
+# ###############
+# ###############
+# # Q3 c
+# ###############
+# ###############
+
 @typechecked
 def evaluate_c_d_pairs(X_train: pd.DataFrame, y_train: pd.DataFrame, X_test: pd.DataFrame, y_test: pd.DataFrame, n_folds: int, c_vals: np.array, d_vals: np.array) -> Tuple[np.array, np.array, np.array, np.array]:
-    '''
-      Return in the order: ERRAVGdcTEST, SuppVect, vmd, MarginT
-      More details about the imlementation are provided in the main function
-      Shape:
-        ERRAVGdcTEST = np array with shape len(d_vals)
-        SuppVect     = np array with shape len(d_vals)
-        vmd          = np array with shape len(d_vals)
-        MarginT      = np array with shape len(d_vals)
-    '''
-    ########################
-    ## Your Solution Here ##
-    ########################
+	'''
+		Return in the order: ERRAVGdcTEST, SuppVect, vmd, MarginT
+		Below are the vectors evaluated by evaluate_c_d_pairs() function
+			ERRAVGdcTEST - Average Testing error for each value of 'd'
+			SuppVect     - Average Number of Support Vectors for each value of 'd'
+			vmd          - Average Number of Support Vectors that Violate the Margin for each value of 'd'
+			MarginT      - Average Value of Hyperplane Margins for each value of 'd'
+		Shape:
+			ERRAVGdcTEST = np array with shape len(d_vals)
+			SuppVect     = np array with shape len(d_vals)
+			vmd          = np array with shape len(d_vals)
+			MarginT      = np array with shape len(d_vals)
+	'''
+	d_val_len = len(d_vals)
+	ERRAVGdcTEST = np.zeros(d_val_len)
+	SuppVect = np.zeros(d_val_len)
+	vmd = np.zeros(d_val_len)
+	MarginT = np.zeros(d_val_len)
+	
+	for d_idx, d in enumerate(d_vals):
+		ERRc = []
+		SuppVectc = []
+		vmdc = []
+		MarginTc = []
+		for c in c_vals:
+			svm = SVC(kernel='poly', degree=d, C=c)
+			errc = []
+			supvectc = []
+			vmdcc = []
+			marginTc = []
+			for fold in range(n_folds):
+				start = fold * (len(X_train) // n_folds)
+				end = (fold + 1) * (len(X_train) // n_folds)
+				X_val = X_train[start:end]
+				y_val = y_train[start:end]
+				X_train_folds = pd.concat([X_train[:start], X_train[end:]])
+				y_train_folds = pd.concat([y_train[:start], y_train[end:]])
+				
+				svm.fit(X_train_folds, y_train_folds)
+				y_pred = svm.predict(X_val)
+				err = mean_absolute_error(y_val, y_pred)
+				errc.append(err)
+				supvectc.append(np.sum(svm.n_support_))
+				vmdcc.append(np.sum(np.abs(svm.decision_function(X_val)) <= 1))
+				marginTc.append(np.mean(np.abs(svm.decision_function(X_val))))
+			
+			ERRc.append(np.mean(errc))
+			SuppVectc.append(np.mean(supvectc))
+			vmdc.append(np.mean(vmdcc))
+			MarginTc.append(np.mean(marginTc))
+		
+		ERRAVGdcTEST[d_idx] = np.min(ERRc)
+		SuppVect[d_idx] = np.mean(SuppVectc)
+		vmd[d_idx] = np.mean(vmdc)
+		MarginT[d_idx] = np.mean(MarginTc)
+	
+	return ERRAVGdcTEST, SuppVect, vmd, MarginT
 
 
 @typechecked
@@ -201,52 +284,63 @@ def plot_test_errors(ERRAVGdcTEST: np.array, d_vals: np.array) -> None:
      Please write the code in below block to generate the graphs as described in the question.
      Note that the code will not be graded, but the graphs submitted in the report will be evaluated.
     '''
-    ########################
-    ## Your Solution Here ##
-    ########################
+    plt.plot(d_vals, ERRAVGdcTEST, marker='o')
+    plt.title('Average Testing Error vs. Polynomial Kernel Degree')
+    plt.xlabel('Degree of Polynomial Kernel')
+    plt.ylabel('Average Testing Error')
+    plt.xticks(d_vals)
+    plt.show()
 
 
-################
-################
-# Q3 d
-################
-################
+# ################
+# ################
+# # Q3 d
+# ################
+# ################
+
 @typechecked
 def plot_avg_support_vec(SuppVect: np.array, d_vals: np.array) -> None:
-    '''
-     Please write the code in below block to generate the graphs as described in the question.
-     Note that the code will not be graded, but the graphs submitted in the report will be evaluated.
-    '''
-    ########################
-    ## Your Solution Here ##
-    ########################
+	'''
+		Please write the code in below block to generate the graphs as described in the question.
+		Note that the code will not be graded, but the graphs submitted in the report will be evaluated.
+	'''
+	plt.plot(d_vals, SuppVect)
+	plt.xlabel('d')
+	plt.ylabel('Average number of support vectors')
+	plt.title('Average number of support vectors vs. d')
+	plt.show()
 
 
 @typechecked
 def plot_avg_violating_support_vec(vmd: np.array, d_vals: np.array) -> None:
-    '''
-     Please write the code in below block to generate the graphs as described in the question.
-     Note that the code will not be graded, but the graphs submitted in the report will be evaluated.
-    '''
-    ########################
-    ## Your Solution Here ##
-    ########################
+	'''
+		Please write the code in below block to generate the graphs as described in the question.
+		Note that the code will not be graded, but the graphs submitted in the report will be evaluated.
+	'''
+	plt.plot(d_vals, vmd)
+	plt.xlabel('d')
+	plt.ylabel('Average number of violating support vectors')
+	plt.title('Average number of violating support vectors vs. d')
+	plt.show()
 
 
-################
-################
-# Q3 e
-################
-################
+# ################
+# ################
+# # Q3 e
+# ################
+# ################
+
 @typechecked
 def plot_avg_hyperplane_margins(MarginT: np.array, d_vals: np.array) -> None:
-    '''
-     Please write the code in below block to generate the graphs as described in the question.
-     Note that the code will not be graded, but the graphs submitted in the report will be evaluated.
-    '''
-    ########################
-    ## Your Solution Here ##
-    ########################
+	'''
+		Please write the code in below block to generate the graphs as described in the question.
+		Note that the code will not be graded, but the graphs submitted in the report will be evaluated.
+	'''
+	plt.plot(d_vals, MarginT, '-o')
+	plt.xlabel('Degree of Polynomial Kernel')
+	plt.ylabel('Average Margin of Hyperplane')
+	plt.title('Average Margin of Hyperplane vs Degree of Polynomial Kernel')
+	plt.show()
 
 
 # # General Instructions
@@ -277,8 +371,8 @@ x, y = read_classification_data(classification_data_2d_path)
     Implement- sigmoid(), cost_function(), cross_entropy_optimizer()
 '''
 
-w: float = 1
-b: float = 1
+w: float = 1.0
+b: float = 1.0
 num_iterations: int = 300
 w, b, costs = cross_entropy_optimizer(w, b, x, y, num_iterations, 0.1)
 print("Weignt W: ", w)
@@ -378,10 +472,8 @@ n_folds = 5
 '''
 Use the results from above and Fill the best c values for d=1,2,3,4
 '''
-########################
-## Your Solution Here ##
-new_c_vals = []
-########################
+new_c_vals = [1, 1, 1, 1, 1]
+
 
 '''
 Below are the vectors evaluated by evaluate_c_d_pairs() function
