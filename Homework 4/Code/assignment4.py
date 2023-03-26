cbbb4a03ba5209e08b47326887ee71b470b0ea04
Huy Lai <lai.huy@tamu.edu>
2023-03-25 23:57:54 -0500

    the hox fix

--- Homework 4/Code/assignment4.py
+++ Homework 4/Code/assignment4.py
@@ -1,11 +1,10 @@
-from sklearn.metrics import mean_absolute_error
-from sklearn.model_selection import KFold
-from sklearn.svm import SVR
+# -*- coding: utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import normalize, StandardScaler
+from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import mean_absolute_error, log_loss
@@ -18,183 +17,184 @@ from typing import Tuple
 random.seed(42)
 np.random.seed(42)
 
+# ###############
+# ###############
+# Q1
+# ###############
+# ###############
 
-# # Q1
 
 @typechecked
 def read_classification_data(file_path: str) -> Tuple[np.array, np.array]:
-	'''
-		Read the data from the path.
-		Return the data as 2 np arrays each with shape (number_of_rows_in_dataframe, 1)
-		Order (np.array from first row), (np.array from second row)
-	'''
+    '''
+      Read the data from the path.
+      Return the data as 2 np arrays each with shape (number_of_rows_in_dataframe, 1)
+      Order (np.array from first row), (np.array from second row)
+    '''
 
-	data = np.loadtxt(file_path, delimiter=',')
-	x = data[0, :].reshape(-1, 1)
-	y = data[1, :].reshape(-1, 1)
-	return x, y
+    data = np.loadtxt(file_path, delimiter=',')
+    x = data[0, :].reshape(-1, 1)
+    y = data[1, :].reshape(-1, 1)
+    return x, y
 
 
 @typechecked
 def sigmoid(s: np.array) -> np.array:
-	'''
-		Return the sigmoid of every number in the array s as an array of floating point numbers
-		sigmoid(s)= 1/(1+e^(-s))
-	'''
-	return 1 / (1 + np.exp(-s))
+    '''
+      Return the sigmoid of every number in the array s as an array of floating point numbers
+      sigmoid(s)= 1/(1+e^(-s))
+    '''
+    return 1 / (1 + np.exp(-s))
 
 
 @typechecked
 def cost_function(w: float, b: float, X: np.array, y: np.array) -> float:
-	'''
-	Inputs definitions:
-		w : weight
-		b : bias
-		X : input  with shape (number_of_rows_in_dataframe, 1)
-		y : target with shape (number_of_rows_in_dataframe, 1)
-	Return the loss as a float data type. 
-	'''
+    '''
+    Inputs definitions:
+      w : weight
+      b : bias
+      X : input  with shape (number_of_rows_in_dataframe, 1)
+      y : target with shape (number_of_rows_in_dataframe, 1)
+    Return the loss as a float data type. 
+    '''
 
-	z = np.dot(X, w) + b
-	yhat = sigmoid(z)
-	return -np.sum(y * np.log(yhat) + (1-y) * np.log(1-yhat))
+    z = np.dot(X, w) + b
+    yhat = sigmoid(z)
+    return -np.sum(y * np.log(yhat) + (1-y) * np.log(1-yhat))
 
 
 @typechecked
-def cross_entropy_optimizer(w: float, b: float, X: np.array, y: np.array, num_iterations: int, alpha: float) -> Tuple[float, float, list]:
-	'''
-		Inputs definitions:
-			w              : initial weight
-			b              : initial bias
-			X              : input  with shape (number_of_rows_in_dataframe, 1)
-			y              : target with shape (number_of_rows_in_dataframe, 1)
-			num_iterations : number of iterations 
-			alpha          : Learning rate
+def cross_entropy_optimizer(w: float, b: float, X: np.array, y: np.array, num_iterations: int, alpha: float) -> (float, float, list):
+    '''
+      Inputs definitions:
+            w              : initial weight
+            b              : initial bias
+            X              : input  with shape (number_of_rows_in_dataframe, 1)
+            y              : target with shape (number_of_rows_in_dataframe, 1)
+            num_iterations : number of iterations 
+            alpha          : Learning rate
 
-		Task: Iterate for given number of iterations and find optimal weight and bias 
-		while also noting the change in cost/ loss after every iteration
+      Task: Iterate for given number of iterations and find optimal weight and bias 
+      while also noting the change in cost/ loss after every iteration
 
-		Make use of the cost_function() above
+      Make use of the cost_function() above
 
-		Return (updated weight, updated bias, list of "costs" after each iteration) in this order
-		"costs" list contains float type numbers  
-	'''
+      Return (updated weight, updated bias, list of "costs" after each iteration) in this order
+      "costs" list contains float type numbers  
+    '''
 
-	costs = []
-	m = X.shape[0]
-	prev = cost_function(w, b, X, y)
+    costs = []
+    m = X.shape[0]
+    prev = cost_function(w, b, X, y)
 
-	for i in range(num_iterations):
-		z = np.dot(X, w) + b
-		a = sigmoid(z)
-		dz = a - y
-		dw = np.dot(X.T, dz) / m
-		db = np.sum(dz) / m
+    for i in range(num_iterations):
 
-		# Update parameters
-		w -= float(alpha * dw)
-		b -= float(alpha * db)
+        z = np.dot(X, w) + b
+        a = sigmoid(z)
+        dz = a - y
+        dw = (1/m) * np.dot(X.T, dz)
+        db = (1/m) * np.sum(dz)
 
-		costs.append(prev)
-		prev = cost_function(w, b, X, y)
+        # Update parameters
+        w -= float(alpha * dw)
+        b -= float(alpha * db)
 
-	return w, b, costs
+        costs.append(prev)
+        prev = cost_function(w, b, X, y)
 
-# # Q3 a
+    return w, b, costs
+################
+################
+# Q3 a
+################
+################
 
 
 @typechecked
 def read_sat_image_data(file_path: str) -> pd.DataFrame:
-	'''
-		Input: filepath to a .csv file
-		Output: Return a DataFrame with the data from the given csv file 
-	'''
+    '''
+      Input: filepath to a .csv file
+      Output: Return a DataFrame with the data from the given csv file 
+    '''
 
-	return pd.read_csv(file_path)
+    return pd.read_csv(file_path)
 
 
 @typechecked
 def remove_nan(df: pd.DataFrame) -> pd.DataFrame:
-	'''
-		Remove nan values from the dataframe and return it
-	'''
+    '''
+      Remove nan values from the dataframe and return it
+    '''
 
-	return df.dropna()
+    return df.dropna()
 
 
 @typechecked
-def normalize_data(Xtrain: pd.DataFrame, Xtest: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+def normalize_data(Xtrain: pd.DataFrame, Xtest: pd.DataFrame) -> (pd.DataFrame, pd.DataFrame):
     '''
-        Normalize each column of the dataframes and Return the dataframes
-        Use sklearn.preprocessing.StandardScaler library to normalize
-        Return the results in the order Xtrain_norm, Xtest_norm
+      Normalize each column of the dataframes and Return the dataframes
+      Use sklearn.preprocessing.StandardScaler library to normalize
+      Return the results in the order Xtrain_norm, Xtest_norm
     '''
+
     scaler = StandardScaler()
-    Xtrain_norm = pd.DataFrame(
-        scaler.fit_transform(Xtrain), columns=Xtrain.columns)
-    Xtest_norm = pd.DataFrame(scaler.transform(Xtest), columns=Xtest.columns)
-    return Xtrain_norm, Xtest_norm
+    return pd.DataFrame(scaler.fit_transform(Xtrain)), pd.DataFrame(scaler.fit_transform(Xtest))
 
 
 @typechecked
 def labels_to_binary(y: pd.DataFrame) -> pd.DataFrame:
     '''
-        Make the lables [1,2,3,4,5] as 0 and [6] as 1
-        Return the DataFrame 
+    Make the lables [1,2,3,4,5] as 0 and [6] as 1
+    Return the DataFrame 
     '''
-    # Create a copy of the original DataFrame to avoid modifying it
-    y_binary: pd.DataFrame = y.copy()
 
-    y_binary.loc[y_binary["Class"].isin([1, 2, 3, 4, 5]), "Class"] = 0
-    y_binary.loc[y_binary["Class"] == 6, "Class"] = 1
+    return y.replace([1, 2, 3, 4, 5], 0).replace([6], 1)
 
-    # Return the modified DataFrame
-    return y_binary
+# ###############
+# ###############
+# Q3 b
+# ###############
+# ###############
 
-
-# # Q3 b
-
-
-# +
 
 @typechecked
 def cross_validate_c_vals(X: pd.DataFrame, y: pd.DataFrame, n_folds: int, c_vals: np.array, d_vals: np.array) -> Tuple[np.array, np.array]:
     '''
-        Return the matrices (ERRAVGdc, ERRSTDdc) in the same order
-        More details about the imlementation are provided in the main function
+      Return the matrices (ERRAVGdc, ERRSTDdc) in the same order
+      More details about the imlementation are provided in the main function
     '''
+
     ERRAVGdc = np.zeros((len(c_vals), len(d_vals)))
     ERRSTDdc = np.zeros((len(c_vals), len(d_vals)))
-    
+
     skf = StratifiedKFold(n_splits=n_folds, shuffle=True)
 
-    for i, c_val in enumerate(c_vals):
-        for j, d_val in enumerate(d_vals):
+    for c_idx, c_val in enumerate(c_vals):
+
+        for d_idx, d_val in enumerate(d_vals):
             err_list = []
-            
-            for train, test in skf.split(X, y):
-                X_train, X_test = X.iloc[train], X.iloc[test]
-                y_train, y_test = y.iloc[train], y.iloc[test]
-                
-                svc = SVC(kernel="poly", degree=d_val, C=c_val)
+
+            for train_index, test_index in skf.split(X, y):
+                X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+                y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+
+                svc = SVC(kernel='poly', degree=d_val, C=c_val)
                 svc.fit(X_train, y_train.values.ravel())
                 y_pred = svc.predict(X_test)
                 err_list.append(mean_absolute_error(y_test, y_pred))
-            
-            ERRAVGdc[i][j] = np.mean(err_list)
-            ERRSTDdc[i][j] = np.std(err_list)
+
+            ERRAVGdc[c_idx][d_idx] = np.mean(err_list)
+            ERRSTDdc[c_idx][d_idx] = np.std(err_list)
 
     return ERRAVGdc, ERRSTDdc
 
 
-# -
-
-@typechecked
 def plot_cross_val_err_vs_c(ERRAVGdc: np.array, ERRSTDdc: np.array, c_vals: np.array, d_vals: np.array) -> None:
 	'''
-		Please write the code in below block to generate the graphs as described in the question.
-		Note that the code will not be graded, but the graphs submitted in the report will be evaluated.
+	 Please write the code in below block to generate the graphs as described in the question.
+	 Note that the code will not be graded, but the graphs submitted in the report will be evaluated.
 	'''
+
 	fig, axs = plt.subplots(len(d_vals), sharex=True,
 							sharey=True, figsize=(10, 10))
 	fig.suptitle('Cross Validation Errors vs. C')
@@ -210,66 +210,63 @@ def plot_cross_val_err_vs_c(ERRAVGdc: np.array, ERRSTDdc: np.array, c_vals: np.a
 	plt.show()
 
 
+# ################
+# ################
 # # Q3 c
+# ################
+# ################
 
-@typechecked
 def evaluate_c_d_pairs(X_train: pd.DataFrame, y_train: pd.DataFrame, X_test: pd.DataFrame, y_test: pd.DataFrame, n_folds: int, c_vals: np.array, d_vals: np.array) -> Tuple[np.array, np.array, np.array, np.array]:
     '''
-        Return in the order: ERRAVGdcTEST, SuppVect, vmd, MarginT
-        Below are the vectors evaluated by evaluate_c_d_pairs() function
-            ERRAVGdcTEST - Average Testing error for each value of 'd'
-            SuppVect     - Average Number of Support Vectors for each value of 'd'
-            vmd          - Average Number of Support Vectors that Violate the Margin for each value of 'd'
-            MarginT      - Average Value of Hyperplane Margins for each value of 'd'
-        Shape:
-            ERRAVGdcTEST = np array with shape len(d_vals)
-            SuppVect     = np array with shape len(d_vals)
-            vmd          = np array with shape len(d_vals)
-            MarginT      = np array with shape len(d_vals)
+    Return in the order: ERRAVGdcTEST, SuppVect, vmd, MarginT
+    More details about the imlementation are provided in the main function
+    Shape:
+      ERRAVGdcTEST = np array with shape len(d_vals)
+      SuppVect     = np array with shape len(d_vals)
+      vmd          = np array with shape len(d_vals)
+      MarginT      = np array with shape len(d_vals)
     '''
-    d_val_len = len(d_vals)
-    ERRAVGdcTEST = np.zeros(d_val_len)
-    SuppVect = np.zeros(d_val_len)
-    vmd = np.zeros(d_val_len)
-    MarginT = np.zeros(d_val_len)
+
+    #     '''
+    #   Below are the vectors evaluated by evaluate_c_d_pairs() function
+    #     ERRAVGdcTEST - Average Testing error for each value of 'd'
+    #     SuppVect     - Average Number of Support Vectors for each value of 'd'
+    #     vmd          - Average Number of Support Vectors that Violate the Margin for each value of 'd'
+    #     MarginT      - Average Value of Hyperplane Margins for each value of 'd'
+    #   Implement- evaluate_c_d_pairs(), plot_test_errors, plot_avg_support_vec(), plot_avg_violating_support_vec(), plot_avg_hyperplane_margins()
+    #   '''
+    # Initialize arrays to store evaluation metrics for different c and d values
+
+    ERRAVGdc, ERRSTDdc = cross_validate_c_vals(
+        X_train, y_train, n_folds, c_vals, d_vals)
+    best_c_idx, best_d_idx = np.unravel_index(
+        ERRAVGdc.argmin(), ERRAVGdc.shape)
+
+    best_c, best_d = c_vals[best_c_idx], d_vals[best_d_idx]
+
+    ERRAVGdcTEST = np.zeros(len(d_vals))
+    SuppVect = np.zeros(len(d_vals))
+    vmd = np.zeros(len(d_vals))
+    MarginT = np.zeros(len(d_vals))
 
     for j, d in enumerate(d_vals):
-        ERRc = []
-        SuppVectc = []
-        vmdc = []
-        MarginTc = []
-        for c in c_vals:
-            svm = SVC(kernel='poly', degree=d, C=c)
-            errc = []
-            supvectc = []
-            vmdcc = []
-            marginTc = []
-            for fold in range(n_folds):
-                start = fold * (len(X_train) // n_folds)
-                end = (fold + 1) * (len(X_train) // n_folds)
-                X_val = X_train[start:end]
-                y_val = y_train[start:end]
-                X_train_folds = pd.concat([X_train[:start], X_train[end:]])
-                y_train_folds = pd.concat([y_train[:start], y_train[end:]])
+        svm = SVC(C=best_c, degree=d, kernel='poly')
+        svm.fit(X_train, y_train.values.ravel())
+        y_pred = svm.predict(X_test)
 
-                svm.fit(X_train_folds, y_train_folds.values.ravel())
-                y_pred = svm.predict(X_val)
-                err = mean_absolute_error(y_val, y_pred)
-                errc.append(err)
-                supvectc.append(np.sum(svm.n_support_))
-                vmdcc.append(np.sum(np.abs(svm.decision_function(X_val)) <= 1))
-                marginTc.append(np.mean(np.abs(svm.decision_function(X_val))))
+        ERRAVGdcTEST[j] = 1 - accuracy_score(y_test, y_pred)
+        SuppVect[j] = len(svm.support_vectors_)
 
-            ERRc.append(np.mean(errc))
-            SuppVectc.append(np.mean(supvectc))
-            vmdc.append(np.mean(vmdcc))
-            MarginTc.append(np.mean(marginTc))
+        vmd[j] = np.sum(np.abs(svm.decision_function(X_test)) < 1)
 
-        ERRAVGdcTEST[j] = np.min(ERRc)
-        SuppVect[j] = np.mean(SuppVectc)
-        vmd[j] = np.mean(vmdc)
-        MarginT[j] = np.mean(MarginTc)
+        w = np.dot(svm.dual_coef_, svm.support_vectors_)
+        margin = 1 / np.linalg.norm(w)
+        MarginT[j] = margin
 
+    # print(ERRAVGdcTEST)
+    # print(SuppVect)
+    # print(vmd)
+    # print(MarginT)
     return ERRAVGdcTEST, SuppVect, vmd, MarginT
 
 
@@ -287,7 +284,12 @@ def plot_test_errors(ERRAVGdcTEST: np.array, d_vals: np.array) -> None:
     plt.show()
 
 
-# # Q3 d
+# ###############
+# ###############
+# Q3 d
+# ###############
+# ###############
+
 
 @typechecked
 def plot_avg_support_vec(SuppVect: np.array, d_vals: np.array) -> None:
@@ -325,7 +327,12 @@ def plot_avg_violating_support_vec(vmd: np.array, d_vals: np.array) -> None:
     plt.show()
 
 
-# # Q3 e
+# ###############
+# ###############
+# Q3 e
+# ###############
+# ###############
+
 
 @typechecked
 def plot_avg_hyperplane_margins(MarginT: np.array, d_vals: np.array) -> None:
@@ -341,10 +348,8 @@ def plot_avg_hyperplane_margins(MarginT: np.array, d_vals: np.array) -> None:
 
 
 # # General Instructions
-# If you want to use a library which is not already included at the top of this file,
-# import them in the function in which you are using the library, not at the top of this file.
-# If you import it at the top of this file, your code will not be evaluated correctly by the autograder.
-# You will not be awarded any points if your code fails because of this reason.
+#
+# If you want to use a library which is not already included at the top of this file, import them in the function in which you are using the library, not at the top of this file. If you import it at the top of this file, your code will not be evaluated correctly by the autograder. You will not be awarded any points if your code fails because of this reason.
 
 # +
 ################
@@ -475,14 +480,9 @@ Implement- evaluate_c_d_pairs(), plot_test_errors, plot_avg_support_vec(), plot_
 ERRAVGdcTEST, SuppVect, vmd, MarginT = evaluate_c_d_pairs(
     Xtrain_norm, ytrain_bin_label, Xtest_norm, ytest_bin_label, n_folds, new_c_vals, d_vals)
 plot_test_errors(ERRAVGdcTEST, d_vals)
-
 # -
-
-# # Q3 d
 
 plot_avg_support_vec(SuppVect, d_vals)
 plot_avg_violating_support_vec(vmd, d_vals)
-
-# # Q3 e
 
 plot_avg_hyperplane_margins(MarginT, d_vals)
